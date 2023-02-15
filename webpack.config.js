61c6a97bff5e5aa6bfa42ddc6325ad2b2552f648
gopeshwark <gopeshwark@gmail.com>
2023-02-16 01:23:05 +0530

    Installed and configured custom webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.export = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: "postcss-loader",
+        options: {
+          ident: "postcss",
+          syntax: "postcss-scss",
+          plugins:() => [
+            require("postcss-import"),
+            require("tailwindcss"),
+            require("autoprefixer"),
+          ],
+        },
+      },
+    ],
+  },
+};
